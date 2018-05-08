3261b3b0afde31d6e54a7c94357bfff4fba3d0b1
Chris Chou <chris.chou@vizexplorer.com>
2018-05-09 10:15:31 +1200

    fix no jira config issue

--- log-work.js
+++ log-work.js
@@ -1,109 +1,113 @@
-var JiraClient = require('jira-connector')
-var FileCookieStore = require('tough-cookie-filestore')
-var request = require('request')
-const fs = require('fs')
-const path = require('path')
-const moment = require('moment')
+var JiraClient = require('jira-connector');
+var FileCookieStore = require('tough-cookie-filestore');
+var request = require('request');
+const fs = require('fs');
+const path = require('path');
+const moment = require('moment');
 
 let username = '';
 
 const _createJiraClient = (protocol, host, port, username, password) => {
-    const jarPath = path.resolve(require('os').homedir(), '.cookies.json')
-    if (!fs.existsSync(jarPath)) fs.closeSync(fs.openSync(jarPath, 'w'))
-    const jar = request.jar(new FileCookieStore(jarPath))
+  const jarPath = path.resolve(require('os').homedir(), '.cookies.json');
+  if (!fs.existsSync(jarPath)) fs.closeSync(fs.openSync(jarPath, 'w'));
+  const jar = request.jar(new FileCookieStore(jarPath));
 
-    return new JiraClient({
-        protocol,
-        host,
-        port,
-        basic_auth: {
-            username: username,
-            password: password
-        },
-        cookie_jar: jar
-    })
-}
+  return new JiraClient({
+    protocol,
+    host,
+    port,
+    basic_auth: {
+      username: username,
+      password: password
+    },
+    cookie_jar: jar
+  });
+};
 
 const _createDefaultJiraClient = () => {
-    const jiraConfigPath = path.resolve(require('os').homedir(), '.jira.config.json')
-    if (fs.existsSync(jiraConfigPath)) {
-        const fileContent = require(jiraConfigPath)
-        username = fileContent.username
+  const jiraConfigPath = path.resolve(
+    require('os').homedir(),
+    '.jira.config.json'
+  );
+  if (fs.existsSync(jiraConfigPath)) {
+    const fileContent = require(jiraConfigPath);
+    username = fileContent.username;
 
-        return _createJiraClient(
-            fileContent.protocol,
-            fileContent.host,
-            fileContent.port,
-            fileContent.username,
-            fileContent.password
-        )
-    } else throw 'no ${HOME}/.jira.config.json'
-}
+    return _createJiraClient(
+      fileContent.protocol,
+      fileContent.host,
+      fileContent.port,
+      fileContent.username,
+      fileContent.password
+    );
+  }
 
-const jira = _createDefaultJiraClient()
+  return null;
+};
 
-const _getUser = (username) => {
-    return jira.user.getUser({ username: 'Chris Chou' })
-        .then((user) => {
-            delete user.timeZone
-            delete user.groups
-            delete user.expand
+const jira = _createDefaultJiraClient();
 
-            return user
-        })
-}
+const _getUser = username => {
+  return jira.user.getUser({ username: 'Chris Chou' }).then(user => {
+    delete user.timeZone;
+    delete user.groups;
+    delete user.expand;
 
-const _formatMomentJira = (aMoment) => {
-    return aMoment.format('Y-MM-DDTHH:mm:ss.SSSZZ')
-}
+    return user;
+  });
+};
 
-const _getStartTime = (timeSpent) => {
-    if (!timeSpent) return
-    const now = moment()
-    if (timeSpent.match(/^\d+$/)) return _formatMomentJira(now.subtract(+timeSpent, 'minutes'))
-    // timespent format: weeks (w), days (d), hours (h) and minutes (m)
-    const weeks = timeSpent.match(/(\d+)w/i)
-    const days = timeSpent.match(/(\d+)d/i)
-    const hours = timeSpent.match(/(\d+)h/i)
-    const minutes = timeSpent.match(/(\d+)m/i)
+const _formatMomentJira = aMoment => {
+  return aMoment.format('Y-MM-DDTHH:mm:ss.SSSZZ');
+};
 
-    // mutate now
-    weeks && now.subtract(weeks[1], 'weeks')
-    days && now.subtract(days[1], 'days')
-    hours && now.subtract(hours[1], 'hours')
-    minutes && now.subtract(minutes[1], 'minutes')
+const _getStartTime = timeSpent => {
+  if (!timeSpent) return;
+  const now = moment();
+  if (timeSpent.match(/^\d+$/))
+    return _formatMomentJira(now.subtract(+timeSpent, 'minutes'));
+  // timespent format: weeks (w), days (d), hours (h) and minutes (m)
+  const weeks = timeSpent.match(/(\d+)w/i);
+  const days = timeSpent.match(/(\d+)d/i);
+  const hours = timeSpent.match(/(\d+)h/i);
+  const minutes = timeSpent.match(/(\d+)m/i);
 
-    return _formatMomentJira(now)
-}
+  // mutate now
+  weeks && now.subtract(weeks[1], 'weeks');
+  days && now.subtract(days[1], 'days');
+  hours && now.subtract(hours[1], 'hours');
+  minutes && now.subtract(minutes[1], 'minutes');
+
+  return _formatMomentJira(now);
+};
 
 const _createWorkLog = (user, timeSpent) => {
-    const worklog = {
-        author: user,
-        timeSpent,
-        started: _getStartTime(timeSpent)
-    }
+  const worklog = {
+    author: user,
+    timeSpent,
+    started: _getStartTime(timeSpent)
+  };
 
-    return worklog
-}
+  return worklog;
+};
 
 const logWork = (issueKey, timeSpent) => {
-    if (issueKey.match(/^[A-Za-z]+-\d+$/)) {
-        return _getUser(username)
-            .then((user) => {
-                const worklog = _createWorkLog(user, timeSpent)
-                return jira.issue.addWorkLog({
-                    issueKey,
-                    worklog,
-                    adjustEstimate: 'auto'
-                })
-            })
-            .catch((err) => console.error('err', err))
-    }
-}
+  if (jira && issueKey.match(/^[A-Za-z]+-\d+$/)) {
+    return _getUser(username)
+      .then(user => {
+        const worklog = _createWorkLog(user, timeSpent);
+        return jira.issue.addWorkLog({
+          issueKey,
+          worklog,
+          adjustEstimate: 'auto'
+        });
+      })
+      .catch(err => console.error('err', err));
+  }
+};
 
 module.exports = {
-    logWork,
-}
-
+  logWork
+};
 
 // logWork('VZA-4140', '1d 2m').then((res) => console.log(res))
